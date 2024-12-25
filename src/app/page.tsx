70b8bbc35170b8bd2f5f1861b8a66ef0378e4f70
nanasi-1 <test-email>
2024-12-25 14:40:10 +0900

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-dvh flex justify-center items-center">
+      <h1 className="text-center text-2xl font-bold">Hello World!</h1>
+    </div>
+  );
+}
