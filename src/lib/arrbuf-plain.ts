fd7f801f5288048560807f30dd7c4ec7e0ef1dd4
nanasi-1 <test-email>
2024-12-25 22:11:04 +0900

    型エラーを修正

--- src/lib/arrbuf-plain.ts
+++ src/lib/arrbuf-plain.ts
@@ -1,5 +1,5 @@
 export function plainTextToArrayBuffer(plainText: string): ArrayBuffer {
-  return new TextEncoder().encode(plainText);
+  return new TextEncoder().encode(plainText) as unknown as ArrayBuffer; // by-pass type check
 }
 
 export function arrayBufferToPlainText(buffer: ArrayBuffer): string {
