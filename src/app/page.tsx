910e4a0705502f1f46680f36cc8b1b3b5a3aacc0
nanasi-1 <test-email>
2024-12-26 16:45:38 +0900

    プライマリカラーを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-dvh flex justify-center items-center flex-col gap-y-3">
-      <h1 className="text-center text-2xl font-bold">Hello World!</h1>
+      <h1 className="text-center text-2xl font-bold text-primary">Hello World!</h1>
       <p>準備中...</p>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          'DEFAULT': '#5ed71d',
+          '50': '#f2fde8',
+          '100': '#e0fbcc',
+          '200': '#c2f79f',
+          '300': '#9bee68',
+          '400': '#77e13a',
+          '500': '#5ed71d',
+          '600': '#409f11',
+          '700': '#337912',
+          '800': '#2c6014',
+          '900': '#275116',
+          '950': '#102d06',
+        },
+        secondary: "#961DD7",
       },
     },
   },
