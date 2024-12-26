5b726c2dd10e17268490cbe24992a6f0f8fce145
ななし <145516449+nanasi-1@users.noreply.github.com>
2024-12-26 17:12:07 +0900

    Setup CSS (#6)
    * プライマリカラーを追加
    
    * secondaryを追加
    
    * 背景色を変更
    
    ---------
    
    Co-authored-by: nanasi-1 <test-email>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 export default function Home() {
   return (
-    <div className="h-dvh flex justify-center items-center flex-col gap-y-3">
-      <h1 className="text-center text-2xl font-bold">Hello World!</h1>
-      <p>準備中...</p>
+    <div className="h-dvh flex justify-center items-center flex-col gap-y-4">
+      <div className="bg-black px-3 py-1">
+        <h1 className="text-center text-3xl font-bold text-primary">Hello World!</h1>
+      </div>
+      <p>暗号掲示板は現在準備中です</p>
+      <p>
+        <a href="https://github.com/nanasi-1/encrypted-board" className="text-secondary underline">GitHub</a>
+      </p>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,38 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // @see https://zenn.dev/hayato94087/articles/9b7f95666806be
+        primary: {
+          'DEFAULT': '#5ed71d',
+          '50': '#f2fde8',
+          '100': '#e0fbcc',
+          '200': '#c2f79f',
+          '300': '#9bee68',
+          '400': '#77e13a',
+          '500': '#5ed71d',
+          '600': '#409f11',
+          '700': '#337912',
+          '800': '#2c6014',
+          '900': '#275116',
+          '950': '#102d06',
+        },
+        secondary: {
+          "DEFAULT": "#bc50fc",
+          '50': '#fbf5ff',
+          '100': '#f6e7ff',
+          '200': '#eed4ff',
+          '300': '#e1b2ff',
+          '400': '#cf80ff',
+          '500': '#bc50fc',
+          '600': '#aa2df0',
+          '700': '#961dd7',
+          '800': '#7c1dac',
+          '900': '#65198a',
+          '950': '#460467',
+        },
+        stone: {
+          '750': '#2E2E2E'
+        }
       },
     },
   },
