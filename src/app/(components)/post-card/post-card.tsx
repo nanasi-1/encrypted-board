b102a17873e7e61a8b9a0608cfef8e4fae9ef75b
nanasi-1 <test-email>
2024-12-28 17:41:35 +0900

    PostCardコンポーネントを作成

---
+++ src/app/(components)/post-card/post-card.tsx
@@ -0,0 +1,9 @@
+/** 各投稿のコンポーネント  */
+// `Post`だと投稿を意味する`post`と名前が被るので`PostCard`とした
+export default function PostCard() {
+  return (
+    <div className="w-5/6 bg-black">
+      Hello World!
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
 import { Icones, LockIcon } from "@/components/ui/icons";
+import PostCard from "./(components)/post-card/post-card";
 
 export default function Home() {
   return (
-    <div className="h-[80vh] justify-center flex items-center flex-col gap-y-4">
-      <div className="bg-black px-3 py-1">
-        <h1 className="text-center text-3xl font-bold text-primary">Hello World!</h1>
+    <>
+      <div className="justify-center flex items-center flex-col gap-y-4 mb-16">
+        <div className="bg-black px-3 py-1">
+          <h1 className="text-center text-3xl font-bold text-primary">Hello World!</h1>
+        </div>
+        <p>
+          <Icones Icon={LockIcon} color="primary" position="left-of-text" />
+          <span>暗号掲示板は現在準備中です</span>
+        </p>
+        <p>
+          <a href="https://github.com/nanasi-1/encrypted-board" target="_blank" className="text-secondary underline">GitHub</a>
+        </p>
       </div>
-      <p>
-        <Icones Icon={LockIcon} color="primary" position="left-of-text" />
-        <span>暗号掲示板は現在準備中です</span>
-      </p>
-      <p>
-        <a href="https://github.com/nanasi-1/encrypted-board" target="_blank" className="text-secondary underline">GitHub</a>
-      </p>
-    </div>
+      <div className="flex items-center flex-col gap-y-7">
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </div>
+    </>
   );
 }
