17339a1609cf4aec977f298da1090cb9136ff5ef
nanasi-1 <test-email>
2024-12-29 11:17:09 +0900

    メニューアイコンをボタンに変更

--- src/app/(components)/post-card/post-card.tsx
+++ src/app/(components)/post-card/post-card.tsx
@@ -24,17 +24,24 @@ export default function PostCard() {
   )
 }
 
-function Menus() {
+function Menus({ onKeyClick, onMoreClick }: {
+  onKeyClick?: () => void,
+  onMoreClick?: () => void,
+}) {
   const KEY_ICON_SIZE = 1.2
-  const MENU_ICON_SIZE = 1.3
+  const MORE_ICON_SIZE = 1.3
 
   return (
     <ul className={styles['menu']}>
       <li>
-        <Icones width={`${KEY_ICON_SIZE}rem`} height={`${KEY_ICON_SIZE}rem`} Icon={KeyIcon} />
+        <button onClick={onKeyClick}>
+          <Icones width={`${KEY_ICON_SIZE}rem`} height={`${KEY_ICON_SIZE}rem`} Icon={KeyIcon} />
+        </button>
       </li>
       <li>
-        <Icones width={`${MENU_ICON_SIZE}rem`} height={`${MENU_ICON_SIZE}rem`} color="white" Icon={MenuDotsIcon} />
+        <button onClick={onMoreClick}>
+          <Icones width={`${MORE_ICON_SIZE}rem`} height={`${MORE_ICON_SIZE}rem`} color="white" Icon={MenuDotsIcon} />
+        </button>
       </li>
     </ul>
   )
