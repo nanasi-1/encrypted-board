2f0339fb021413cc9b807ebd2ff72ac569b6072f
nanasi-1 <test-email>
2025-01-01 10:27:29 +0900

    アイコンを追加

--- src/app/(components)/post-button.tsx
+++ src/app/(components)/post-button.tsx
@@ -1,15 +1,17 @@
-import { Icones, LockIcon } from "@/components/ui/icons"
+import { Icones, AddEncryptionIcon } from "@/components/ui/icons"
 
-function UI() {
+function UI({ Icon }: {
+  Icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+}) {
   return (
     <button className="border-primary border-2">
-      <Icones Icon={LockIcon} />
+      <Icones Icon={Icon} />
     </button>
   )
 }
 
 export default function PostButton() {
   return (
-    <UI />
+    <UI Icon={AddEncryptionIcon} />
   )
 }
