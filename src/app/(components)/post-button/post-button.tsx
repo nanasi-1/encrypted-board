4510e0b0cba5fbe90fb760ed51d79acd61e9119c
nanasi-1 <test-email>
2025-01-01 10:41:28 +0900

    ボタンの見た目を作成

--- src/app/(components)/post-button/post-button.tsx
+++ src/app/(components)/post-button/post-button.tsx
@@ -1,11 +1,18 @@
 import { Icones, AddEncryptionIcon } from "@/components/ui/icons"
 import styles from './post-button.module.css'
+import { classnameBuilder } from "@/utils/classname-builder"
 
-function UI({ Icon }: {
+function UI({ Icon, className }: {
   Icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+  className?: string
 }) {
+  const _className = classnameBuilder()
+    .add(styles['button'])
+    .add(className)
+    .build()
+
   return (
-    <button className={styles['button']}>
+    <button className={_className}>
       <Icones Icon={Icon} fontSize={24} />
     </button>
   )
@@ -13,6 +20,6 @@ function UI({ Icon }: {
 
 export default function PostButton() {
   return (
-    <UI Icon={AddEncryptionIcon} />
+    <UI Icon={AddEncryptionIcon} className="bottom-10 right-10" />
   )
 }
