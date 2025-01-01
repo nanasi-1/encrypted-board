c3389b311f53ab4e6cbef01853ec56ff581e606c
nanasi-1 <test-email>
2025-01-01 20:54:13 +0900

    PaginationのpropsをhasNextに変更

--- src/app/(components)/posts.tsx
+++ src/app/(components)/posts.tsx
@@ -4,12 +4,14 @@ import PostCard from "./post-card/post-card";
 export default async function Posts({ currentPage }: {
   currentPage: number
 }) {
+  const hasNext = currentPage < 4 // 仮
+
   return (
     <div className="flex items-center flex-col gap-y-7">
       <PostCard />
       <PostCard />
       <PostCard />
-      <Pagination currentPage={currentPage} maxPage={4} baseHref="/?page=" />
+      <Pagination currentPage={currentPage} hasNext={hasNext} baseHref="/?page=" />
     </div>
   )
 }
