2ca8cf8e6a871d5beefcb3e8ea1b76e985a3764d
nanasi-1 <test-email>
2025-01-02 12:52:05 +0900

    useModalはchildrenを受け取らない

--- src/components/ui/modal/index.tsx
+++ src/components/ui/modal/index.tsx
@@ -5,13 +5,14 @@ import ModalUI from './ModalUI'
 
 // モーダルを制御する関数付きフック
 // TODO 複数のモーダルが一度に開かれた時どうするか考える
-export function useModal(children?: React.ReactNode) {
+export function useModal() {
   const { setModalComponent, dialogRef } = useModalContext()
 
-  const open = () => {
+  const open = (children: React.ReactNode) => {
     setModalComponent(<ModalUI onClose={close}>{children}</ModalUI>)
     dialogRef.current?.showModal()
   }
+
   const close = () => dialogRef.current?.close()
 
   return { open, close } // 基本はopenのみを使う
