e0135b39474372c7db933e6dbd8850eebb7b97b6
nanasi-1 <test-email>
2025-01-02 12:16:16 +0900

    不要なものを削除

--- src/components/ui/modal/index.tsx
+++ src/components/ui/modal/index.tsx
@@ -1,21 +1,14 @@
 'use client'
 
-import { useRef, useState } from 'react'
 import { Icones, CloseIcon } from '../icons'
 import styles from './index.module.css'
 import { useModalContext } from './context'
 
-export function ModalUI({ children, onClose, dialogRef }: {
+export function ModalUI({ children, dialogRef }: {
   children?: React.ReactNode
   dialogRef?: React.RefObject<HTMLDialogElement | null>
-  onClose?: () => void
 }) {
-  const closeModal = () => {
-    console.log('click')
-    console.log(dialogRef?.current)
-    dialogRef?.current?.close()
-    onClose?.()
-  }
+  const closeModal = () => dialogRef?.current?.close()
 
   // TODO align-centerをどうするか考える
   return (
@@ -36,18 +29,13 @@ export function ModalUI({ children, onClose, dialogRef }: {
 // モーダルを制御する関数付きフック
 // TODO 複数のモーダルが一度に開かれた時どうするか考える
 export function useModal(children?: React.ReactNode) {
-  const [isOpen, setIsOpen] = useState(false)
   const { setModalComponent, dialogRef } = useModalContext()
 
-  const close = () => {
-    console.log('close modal')
-  }
   const open = () => {
-    console.log('open modal')
     setModalComponent(children)
-    console.log(dialogRef.current)
     dialogRef.current?.showModal()
   }
 
+  const close = () => dialogRef.current?.close()
   return { open, close } // 基本はopenのみを使う
 }
