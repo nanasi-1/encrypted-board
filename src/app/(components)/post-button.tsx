d2c7120e85c5e4d010e1702bfd9ee322bc35da9d
nanasi-1 <test-email>
2025-01-02 16:54:55 +0900

    PostModal用ファイルを作成

--- src/app/(components)/post-button.tsx
+++ src/app/(components)/post-button.tsx
@@ -2,15 +2,7 @@
 import { AddEncryptionIcon } from "@/components/ui/icons"
 import RoundedIconButton from "@/components/ui/rounded-icon-button"
 import { useModal } from "@/components/ui/modal"
-
-function PostModal() {
-  return (
-    <p>
-      Happy New Year! <br />
-      投稿するためのフォームがここにくるはず<br />
-    </p>
-  )
-}
+import PostModal from "./post-modal"
 
 export default function PostButton() {
   const { open: openModal } = useModal()
---
+++ src/app/(components)/post-modal.tsx
@@ -0,0 +1,14 @@
+export default function PostModal() {
+  return (
+    <div>
+      <h2>投稿</h2>
+      <p className="mb-2">
+        Happy New Year! <br />
+        もう2025年ってまじ？<br />
+      </p>
+      <p>
+        投稿するためのフォームがここにくるはず<br />
+      </p>
+    </div>
+  )
+}
