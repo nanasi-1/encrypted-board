a2aa485bfb5305dfb1db3cad0819f11f75bb62a1
nanasi-1 <test-email>
2025-01-02 17:19:02 +0900

    簡易的なフォームを作成

--- src/app/(components)/post-modal.tsx
+++ src/app/(components)/post-modal.tsx
@@ -1,16 +1,28 @@
+'use client'
+
 import { ModalTitle } from "@/components/ui/modal/ModalUI";
+import { FormEventHandler } from "react";
+
+const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+  console.log(event)
+  debugger
+}
 
 export default function PostModal() {
   return (
-    <div>
+    <>
       <ModalTitle>投稿</ModalTitle>
-      <p className="mb-2">
-        Happy New Year! <br />
-        もう2025年ってまじ？<br />
-      </p>
-      <p>
-        投稿するためのフォームがここにくるはず<br />
-      </p>
-    </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="">平文</label>
+          <input type="text" required />
+        </div>
+        <div>
+          <label htmlFor="">公開鍵</label>
+          <input type="text" required />
+        </div>
+        <button type="submit">投稿</button>
+      </form>
+    </>
   )
 }
