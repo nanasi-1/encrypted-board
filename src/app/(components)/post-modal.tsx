3de377cec602099bb7654e356e2d5e943ee5d7a8
nanasi-1 <test-email>
2025-01-02 17:02:21 +0900

    タイトル用コンポーネントを作成

--- src/app/(components)/post-modal.tsx
+++ src/app/(components)/post-modal.tsx
@@ -1,7 +1,9 @@
+import { ModalTitle } from "@/components/ui/modal/ModalUI";
+
 export default function PostModal() {
   return (
     <div>
-      <h2>投稿</h2>
+      <ModalTitle>投稿</ModalTitle>
       <p className="mb-2">
         Happy New Year! <br />
         もう2025年ってまじ？<br />
