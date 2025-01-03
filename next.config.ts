d1226363a57a4c8609d8d1f6449942baa75d70e8
nanasi-1 <test-email>
2025-01-03 19:38:43 +0900

    next.config.tsを変更

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    URL: process.env.VERCEL_URL 
+      ? `https://${process.env.VERCEL_URL}`
+      : 'http://localhost:3000',
+    API_ENDPOINT: process.env.VERCEL_URL 
+      ? `https://${process.env.VERCEL_URL}/api`
+      : 'http://localhost:3000/api',
+  }
 };
 
 export default nextConfig;
