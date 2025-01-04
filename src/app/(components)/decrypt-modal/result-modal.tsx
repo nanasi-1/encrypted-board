1f088682d592953152d94c64dff36ed756c59b1f
nanasi-1 <test-email>
2025-01-04 13:47:10 +0900

    checkのアイコンを追加

--- src/app/(components)/decrypt-modal/result-modal.tsx
+++ src/app/(components)/decrypt-modal/result-modal.tsx
@@ -1,4 +1,5 @@
 import { SubmitButton } from "@/components/ui/form"
+import { CheckIcon, Icones } from "@/components/ui/icons"
 import { useModal } from "@/components/ui/modal"
 import { ModalTitle } from "@/components/ui/modal/ModalUI"
 
@@ -9,7 +10,10 @@ export default function ResultModal({ plainText }: {
 
   return (
     <div>
-      <ModalTitle>復号完了</ModalTitle>
+      <ModalTitle>
+        <Icones Icon={CheckIcon} fontSize={28} position="left-of-text" />
+        復号完了
+      </ModalTitle>
       <div className="mb-7">
         <h3 className="font-bold mb-6">平文</h3>
         <div className="border-primary-700 border-y break-words px-3 py-3 min-h-28">
