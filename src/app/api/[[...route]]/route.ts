c96553e933abaef5a85c2dd4a85eed0d3b1d2d7d
nanasi-1 <test-email>
2025-01-06 16:10:53 +0900

    参考にしたリンクを追加

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,6 +1,8 @@
 import app from "@/api";
 import { handle } from "hono/vercel";
 
+// @see https://hono.dev/docs/getting-started/vercel#node-js
+
 export const GET = handle(app)
 export const POST = handle(app)
 export const PUT = handle(app)
