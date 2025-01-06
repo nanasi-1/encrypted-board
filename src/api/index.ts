2105c0e541eeaa86df45c8189583348311d1cee2
nanasi-1 <test-email>
2025-01-06 17:31:11 +0900

    最後の/を区別しないよう修正

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,9 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import routes from './routes'
 
-const app = new Hono().basePath('/api')
+const app = new Hono({ 
+  strict: false // 最後の/を区別しない @see https://hono.dev/docs/api/hono#strict-mode
+}).basePath('/api')
   .use(logger())
   .route('/', routes)
 
