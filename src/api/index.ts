f9c77e21eab753b8c1cc53758733c5b3602473a5
nanasi-1 <test-email>
2025-01-06 16:10:06 +0900

    APIサーバーのみで起動できるように

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 
 const app = new Hono().basePath('/api')
+  .use(logger())
 
 export type AppType = typeof app
 export default app
