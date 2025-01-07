98b5e7be69e7e2a1a415765897a845c6c5355ae5
nanasi-1 <test-email>
2025-01-07 18:18:36 +0900

    署名鍵の生成関数を追加

--- src/lib/generate-key.ts
+++ src/lib/generate-key.ts
@@ -1,16 +1,32 @@
-async function generateEncryptKey() {
+export async function generateEncryptKey() {
   const algorithm: RsaHashedKeyGenParams = {
     name: "RSA-OAEP",
     modulusLength: 1024,
     publicExponent: new Uint8Array([1, 0, 1]),
     hash: { name: "SHA-256" }
   }
-  const pair = await globalThis.crypto.subtle.generateKey(algorithm, true, ['encrypt', 'decrypt'])
+  const pair = await globalThis.crypto.subtle.generateKey(
+    algorithm,
+    true,
+    ['encrypt', 'decrypt']
+  )
   return pair
 }
 
-async function generateSignKey() {
-  
+export async function generateSignKey() {
+  const algorithm: RsaHashedKeyGenParams = {
+    name: "RSA-PSS",
+    modulusLength: 1024,
+    publicExponent: new Uint8Array([1, 0, 1]),
+    hash: { name: "SHA-256" }
+  }
+  const pair = await globalThis.crypto.subtle.generateKey(
+    algorithm,
+    true,
+    ['sign', 'verify']
+  )
+  return {
+    signKey: pair.privateKey,
+    verifyKey: pair.publicKey
+  }
 }
-
-export {}
