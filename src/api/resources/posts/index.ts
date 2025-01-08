07eb758fee4a6600d3919e8a95fbec12edb6287c
nanasi-1 <test-email>
2025-01-08 17:50:19 +0900

    ipアドレスの取得チェック

--- src/api/resources/posts/index.ts
+++ src/api/resources/posts/index.ts
@@ -1,16 +1,32 @@
 import { Hono } from "hono";
 import { getPostsByPage } from "./features/get-all";
 import { parsePostQuery } from "./scheme";
+import { createPost } from "./features/create";
+import type { CreatePostRequired } from "./types";
+
+import { getConnInfo as getLocalConnInfo } from "hono/bun"; // local
+import { getConnInfo } from "hono/vercel"; // 本番
 
 const app = new Hono()
   .get('/', async c => {
     const { page } = parsePostQuery(c.req.query())
     return c.json(await getPostsByPage(page))
   })
-  .post('/', c => {
-    return c.json({
-      message: '投稿APIは準備中です'
-    })
+  .post('/', async c => {
+    const { remote: { address } } = !!process.env.VERCEL_ENV
+     ? getConnInfo(c)
+     : getLocalConnInfo(c)
+     
+    const body = await c.req.json()
+
+    const post: CreatePostRequired = {
+      ...body,
+      ipAddress: c.req
+    }
+
+    // await createPost(post)
+
+    return c.json(address)
   })
 
 export default app
