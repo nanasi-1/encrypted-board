ddbd167e307fb3c94b4b0b1ffd36788443123805
nanasi-1 <test-email>
2025-01-11 18:27:32 +0900

    プロダクションビルド時はVERCEL_PROJECT_PRODUCTION_URLを参照

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from "next";
 
+// @see https://vercel.com/docs/projects/environment-variables/system-environment-variables
+const baseUrl = process.env.VERCEL_ENV === 'production'
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : 'http://localhost:3000'
+
 const nextConfig: NextConfig = {
   env: {
-    URL: process.env.VERCEL_URL 
-      ? `https://${process.env.VERCEL_URL}`
-      : 'http://localhost:3000',
-    API_ENDPOINT: process.env.VERCEL_URL 
-      ? `https://${process.env.VERCEL_URL}/api`
-      : 'http://localhost:3000/api',
+    URL: baseUrl,
+    API_ENDPOINT: `${baseUrl}/api`,
     RUNTIME: 'Next.js'
   }
 };
