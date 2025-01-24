e0d51182875b62e5a9ecbe54166a7ea073a93f9a
nanasi-1 <test-email>
2025-01-24 21:13:05 +0900

    ボタンを作成

---
+++ src/app/(components)/default-keys/show-button.tsx
@@ -0,0 +1,8 @@
+import { KeyIcon } from "@/components/ui/icons";
+import RoundedIconButton from "@/components/ui/rounded-icon-button";
+
+export default function ShowDefaultKeyButton() {
+  return (
+    <RoundedIconButton Icon={KeyIcon} />
+  )
+}
--- src/app/(components)/fixed-buttons.tsx
+++ src/app/(components)/fixed-buttons.tsx
@@ -1,10 +1,12 @@
 // 右下のボタンのコンテナ
 import PostButton from "./create-post/post-button";
+import ShowDefaultKeyButton from "./default-keys/show-button";
 
 export default function FixedButtons(/* 各ボタンに必要なprops */) {
   return (
-    <div className="fixed bottom-10 right-10 z-10">
+    <div className="fixed bottom-10 right-10 z-10 flex flex-col gap-y-4">
       <PostButton />
+      <ShowDefaultKeyButton />
     </div>
   )
 }
