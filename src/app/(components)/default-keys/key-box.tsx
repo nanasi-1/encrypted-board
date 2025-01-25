94864e552ac444cc7570d7070f6750cdc49aa241
nanasi-1 <test-email>
2025-01-25 14:41:30 +0900

    コピー機能を作成

--- src/app/(components)/default-keys/key-box.tsx
+++ src/app/(components)/default-keys/key-box.tsx
@@ -1,10 +1,49 @@
-import { ClipboardIcon, CheckIcon, Icones } from "@/components/ui/icons";
+'use client'
+
+import { ClipboardIcon, CheckIcon, CloseIcon, Icones } from "@/components/ui/icons";
 import { TextBox } from "@/components/ui/text-box";
 import Cipher from "@/components/ui/cipher";
 import styles from './index.module.css'
+import { useEffect, useState } from "react";
+
+function CopyButton({ target, className, iconClassname }: {
+  className?: string
+  iconClassname?: string
+  target: string
+}) {
+  const [status, setStatus] = useState<'pending' | 'done' | 'error'>('pending')
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(target)
+      .then(() => setStatus('done'))
+      .catch(e => {
+        console.error(e)
+        setStatus('error')
+      })
+  }
+
+  useEffect(() => {
+    if (status === 'pending') return
+    const id = setTimeout(() => setStatus('pending'), 1000)
+    return () => clearTimeout(id)
+  }, [status])
+  
+  return (
+    <button className={className} onClick={handleClick}>
+      <Icones 
+        color="black" 
+        className={"text-2xl " + iconClassname} 
+        Icon={status === 'pending' 
+          ? ClipboardIcon 
+          : status === 'done' ? CheckIcon : CloseIcon
+        } 
+      />
+    </button>
+  )
+}
 
 export default function KeyBox({ label, children, className }: {
-  children: React.ReactNode
+  children: string
   className?: string
   label: React.ReactNode
 }) {
@@ -14,9 +53,11 @@ export default function KeyBox({ label, children, className }: {
       <div className="my-2 max-h-[1.4rem] overflow-y-scroll">
         <Cipher color="white">{children}</Cipher>
       </div>
-      <button className="bg-primary text-black font-bold">
-        <Icones color="black" className="text-2xl" Icon={ClipboardIcon} />
-      </button>
+      {/* Clipboard APIの存在チェック */}
+      {typeof window?.navigator?.clipboard?.writeText === 'function' ?
+        <CopyButton target={children} className="bg-primary text-black font-bold" />
+        : null
+      }
     </TextBox>
   )
 }
