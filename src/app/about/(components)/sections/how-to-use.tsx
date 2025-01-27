625d718060d0afc8a7b4b221b553ba8e918dc0cb
nanasi-1 <test-email>
2025-01-27 21:54:43 +0900

    見出しを作成

---
+++ src/app/about/(components)/sections/how-to-use.tsx
@@ -0,0 +1,20 @@
+import { Section, Section2 } from "../ui";
+
+export default function HowToUse() {
+  return (
+    <Section
+      id="how-to-use"
+      title="暗号掲示板の使い方"
+    >
+      <Section2 title="デフォルト鍵" id="default-key">
+        執筆中...
+      </Section2>
+      <Section2 title="平文を投稿する">
+        <p>執筆中...</p>
+      </Section2>
+      <Section2 title="暗号文を復号する">
+        <p>執筆中...</p>
+      </Section2>
+    </Section>
+  )
+}
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,7 @@ import './index.css'
 import AboutEncryptedBoard from './(components)/sections/about-encrypted-board'
 import Introduction from './(components)/sections/introduction'
 import AboutCrypto from './(components)/sections/about-crypto'
+import HowToUse from './(components)/sections/how-to-use'
 
 export default function Page() {
   return (
@@ -9,6 +10,7 @@ export default function Page() {
       <Introduction />
       <AboutEncryptedBoard />
       <AboutCrypto />
+      <HowToUse />
     </div>
   )
 }
