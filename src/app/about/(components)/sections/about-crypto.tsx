97638df51d6f8981f4154ee729eb5b3ea705acd8
nanasi-1 <test-email>
2025-01-27 21:36:16 +0900

    Header3とSection2を作成

--- src/app/about/(components)/sections/about-crypto.tsx
+++ src/app/about/(components)/sections/about-crypto.tsx
@@ -1,11 +1,11 @@
 import Cipher from "@/components/ui/cipher";
-import { ColorStrong, Section } from "../ui";
+import { ColorStrong, Header3, Section, Section2 } from "../ui";
 
 export default function AboutCrypto() {
   return (
     <Section id="about-crypto" title="暗号とは">
       {/* 一応がんばってはみましたが、それでも間違いはたくさんあると思うので、指摘してくださると助かります。*/}
-      <section>
+      <Section2>
         <p>
           そもそも暗号とは、<strong>特別な知識なしでは読めない文章</strong>のことです。<br />
           暗号文は多くの場合、<ColorStrong>秘匿</ColorStrong>を目的として暗号化されています。<br />
@@ -17,22 +17,26 @@ export default function AboutCrypto() {
         </p>
         <p>
           しかし、たぬき暗号は近くにたぬきの絵などのヒントがありますが、実際の暗号は<strong>解読されてはいけません</strong>。<br />
-          ということは、復号方法は隠し通す必要があると思うかもしれません。<br />
-          それは決して間違ってはいないのですが、実はより優れた方法があります。<br />
+          ということは復号方法は隠し通すものだと考えるかもしれませんが、実はより優れた方法があります。<br />
           それは<strong>鍵</strong>を使った暗号方式です。<br />
         </p>
         <div className="block leading-6 absolute right-0 w-2/3 top-3 text-sm opacity-30 select-none">
           <Cipher>TQ4UMc9vAgY+OekbQ9pjh/Js25gj7Mkry3+L0s2L/ztPoMNudB5WFpORGPY7zyWSDT6DrwHtf7Ejy6rvStAJoWH2+gHOwE6t5P+TvcrMMq/sWOK68s0SbdXNCV6tMQVWzr9Mg77fOFamG8fVTzJpL2vrlIf8+NLIu94MzcMUCbNQIDfYBfr91Lh9fXipfjbNwTL/vuKjtAiRDBVZ2wXYL5nA7nM82B2Y44Qc6LCglcGJRx6j1AR6pfaAGUsgodJNaNC6jGCjSm62CGhSNpWwjcbog4h0VnjvHq3VOXXRGGgF38jpLuS9gHebXa2hyshKEDZybZB9myHzI27bAMNZ0Q==</Cipher>
         </div>
-      </section>
-      <section>
-        <h3>まとめ</h3>
+      </Section2>
+      <Section2 title="暗号においての鍵">
+        <p>執筆中...</p>
+      </Section2>
+      <Section2 title="公開鍵暗号">
+        <p>執筆中...</p>
+      </Section2>
+      <Section2 title="要約">
         <ul className="list-disc ml-5">
           <li>第三者が文を見ても特別な知識なしでは読めない秘匿された文</li>
           <li>相手に暗号化ツールを知られても、鍵が知られなければ安全</li>
           <li>最近はコンピューターでの暗号化が主流</li>
         </ul>
-      </section>
+      </Section2>
     </Section>
   )
 }
--- src/app/about/(components)/ui/index.tsx
+++ src/app/about/(components)/ui/index.tsx
@@ -11,6 +11,14 @@ export function Header2({ children, className }: UIProps) {
   )
 }
 
+export function Header3({ children, className }: UIProps) {
+  return (
+    <h3 className={`text-primary font-bold text-xl mb-4 ${className ?? ''}`}>
+      <span className="text-primary font-bold text-[1.35rem] bg-black px-2 py-1.5">{children}</span>
+    </h3>
+  )
+}
+
 export function ColorStrong({ children, color, className }: UIProps & {
   color?: string
 }) {
@@ -27,6 +35,20 @@ export function Section({ children, className, title, id }: UIProps & {
     <section id={id} className={`mb-10 ${className ?? ''}`}>
       <Header2 className="mb-5">{title}</Header2>
       <div className="px-3 relative">
+        {children}
+      </div>
+    </section>
+  )
+}
+
+export function Section2({ children, className, title, id }: UIProps & {
+  title?: React.ReactNode
+  id?: string
+}) {
+  return (
+    <section id={id} className={`mb-5 ${className ?? ''}`}>
+      {title ? <Header3 className="mb-3">{title}</Header3> : null}
+      <div className="relative">
         {children}
       </div>
     </section>
