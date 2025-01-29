c355dbcb8eeeec1e89e00cd2fb7541897e507e8f
nanasi-1 <test-email>
2025-01-29 16:02:43 +0900

    暗号掲示板の使い方を執筆

--- src/app/about/(components)/sections/how-to-use/index.tsx
+++ src/app/about/(components)/sections/how-to-use/index.tsx
@@ -1,7 +1,26 @@
 import Link from "next/link";
 import { EffectWrapper, Section, Section2 } from "../../ui";
-import { Icones, KeyIcon } from "@/components/ui/icons";
+import { AddEncryptionIcon, Icones, KeyIcon, LockIcon } from "@/components/ui/icons";
 import ShowDefaultKeyButton from "@/app/(components)/default-keys/show-button";
+import Cipher from "@/components/ui/cipher";
+import PostCard from "@/app/(components)/post-card/post-card";
+import { PostData } from "@/types";
+
+const dummyPost: PostData = {
+  body: 'UYe4wsQiJA5RzaAvREd7GjzmkIfUwYOD28nBtnm485Vdtu2ZOAc1vmB60nuk2MCQGurTjNeO7U5FuvnHFKPtTqlzOsIGXCJvPoxYQlI8LFdhoAVJpZrNLlrJ9eTbreui0F70iB8PAvQxFbvF8O/WhD0vwdbDjz6Bhmuw+1NQn9LC19rC5p6Rm3E4DzUczVVIa7g5uzwXDGVP8cYof1lkwl3oLfcJWFoVH79Cx2sMEM59UxRkOTWES5/npmZ7XDbCjwUSGudpC4SZXHS0nYHY+tIymt8GzavY5hYAWef1F73i24xkn5a/M+0624nTXgw19w1E8Yjzih8bwO85V6sGYA==',
+  publicKeyDigest: 'RT7o2GZz1+Sqn9sZqmUrobOqDeU=',
+  id: 0,
+  createdAt: new Date().toString(),
+  sign: { has: false }
+}
+
+const dummyPost2: PostData = {
+  body: 'ここに暗号文が表示されます',
+  publicKeyDigest: '公開鍵のハッシュ',
+  id: 0,
+  createdAt: new Date().toString(),
+  sign: { has: false }
+}
 
 export default function HowToUse() {
   return (
@@ -41,20 +60,52 @@ export default function HowToUse() {
           を参考にしてください。<br />
         </p>
       </Section2>
-      <Section2 title="平文を投稿する">
+      <Section2 title="暗号文を投稿する">
+        <p>
+          暗号文の投稿には以下の2つが必要です。<br />
+        </p>
         <ul>
-          <li>右下のボタンから投稿できる</li>
-          <li>平文と公開鍵（デフォルト鍵からコピペしてもOK）を入力する必要がある</li>
-          <li>当掲示板では鍵の識別にハッシュを使っていて、これは投稿に表示される</li>
+          <li>暗号化のための<strong>公開鍵</strong>（デフォルト鍵を使用可能）</li>
+          <li>伝えたいメッセージである<strong>平文</strong></li>
         </ul>
+        <p>
+          投稿するには、<Link href="/">トップページ</Link>の右下にある
+          <Icones Icon={AddEncryptionIcon} />ボタンを押し、これらの情報を入力します。<br />
+          そして下にある「投稿」ボタンを押すことで、投稿が完了します。<br />
+        </p>
+        <div className="mb-5">
+          <PostCard post={dummyPost2} />
+        </div>
+        <p>
+          なお、当掲示板では鍵の識別に<strong>ハッシュ</strong>を利用しています。<br />
+          ハッシュは鍵ごとに一意な文字列で、例えば<Cipher>IL2ui4JVR40tlIE47CRbIHqW1cY=</Cipher>という感じです。<br />
+          暗号化に使用された鍵のハッシュは、投稿の右上の
+          <Icones Icon={LockIcon} />
+          から確認できます。<br />
+        </p>
       </Section2>
-      <Section2 title="暗号文を復号する">
-        <ul>
-          <li>公開鍵と対の秘密鍵を使うことで復号できる</li>
-          <li>正しい鍵が入力された場合は平文が表示される</li>
-          <li>鍵が合わない場合はエラーが出て、それぞれの鍵のハッシュが表示される</li>
-          <li>掲示板の投稿にはハッシュがあるので、それを見てどの鍵を使うべきか判断する</li>
-        </ul>
+      <Section2 title="復号する">
+        <p>
+          秘密鍵を知っている投稿は<strong>復号して平文を読む</strong>ことが可能です。<br />
+        </p>
+        <p>
+          各投稿の暗号化に使用された公開鍵は、ハッシュを確認することで識別できます。<br />
+          そのハッシュの秘密鍵を知っている（デフォルト鍵も含む）場合、
+          <strong>投稿右上の<Icones Icon={KeyIcon} />を押して秘密鍵を入力</strong>
+          することで、復号して平文を閲覧できます。<br />
+        </p>
+        <div className="mb-5">
+          <PostCard post={dummyPost} />
+        </div>
+        <p>
+          例えば、こちらの投稿を見てみます。<br />
+          この暗号文の公開鍵のハッシュは<Cipher>RT7o2GZz1+Sqn9sZqmUrobOqDeU=</Cipher>です。<br />
+          そして、このハッシュの鍵は<a href="#default-key">デフォルト鍵</a>に含まれているため、<strong>この暗号は復号可能</strong>です。<br />
+        </p>
+        <p>
+          復号するには、デフォルト鍵から該当する鍵を探して<strong>秘密鍵をコピー</strong>します。<br />
+          そして<Icones Icon={KeyIcon} />を押して秘密鍵を入力すると、平文が表示されるはずです。<br />
+        </p>
       </Section2>
     </Section>
   )
